5374bf0c2f3aff289f913bb01ef99535438ecd99
Pratik Nugurwar <pratiknugurwar1@gmail.com>
2025-07-27 15:39:57 +0530

    Lint error fixed

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // tells Next.js to use static export
-  trailingSlash: true,
 };
 
 export default nextConfig;
